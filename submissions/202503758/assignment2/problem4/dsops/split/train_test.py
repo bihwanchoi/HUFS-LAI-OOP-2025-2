a5f073dd1ef0d7f2212ad2ea13e6358192df1b90
bihwanchoi <84217296+bihwanchoi@users.noreply.github.com>
2025-09-25 15:16:39 +0900

    problem4 수정

--- submissions/202503758/assignment2/problem4/dsops/split/train_test.py
+++ submissions/202503758/assignment2/problem4/dsops/split/train_test.py
@@ -2,7 +2,7 @@ import random
 
 def train_test_split(seq: list, test_ratio: float, seed: int | None = None) -> tuple[list, list]:
     if not (0.0 <= test_ratio <= 1.0): raise(ValueError)
-    if (not seq) or (test_ratio == 0.0 or test_ratio == 1.0): return [], []
+    if not seq: return [], []
     
     copy = seq.copy()
     if seed is not None:
